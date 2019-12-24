bbe6f9e8bee512c1a9c41a459ca0d93141123112
Andrew <ahoffman@venadotech.com>
2019-12-24 14:25:33 -0600

    Add project files.

---
+++ SharpRemote/SharpRemote/ViewModels/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpRemote.ViewModels
+{
+    internal class BaseViewModel
+    {
+    }
+}
---
+++ SharpRemote/SharpRemote/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Sockets.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SharpRemote.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            ButtonPressedCommand = new Command<string>(async (parameter) => await OnButtonPressed(parameter));
+        }
+
+        public ICommand ButtonPressedCommand { get; private set; }
+
+        private async Task OnButtonPressed(string parameter)
+        {
+            var address = "192.168.1.123";
+            var port = 10002;
+            var command = $"RCKY{parameter}  \r";
+
+
+            var client = new TcpSocketClient();
+            await client.ConnectAsync(address, port);
+
+            // we're connected!
+
+            var bytes = Encoding.ASCII.GetBytes(command);
+
+            // write to the 'WriteStream' property of the socket client to send data
+            await client.WriteStream.WriteAsync(bytes, 0, bytes.Length);
+            await client.WriteStream.FlushAsync();
+
+
+            var thing = client.GetConnectedInterfaceAsync();
+            await client.DisconnectAsync();
+        }
+    }
+}
