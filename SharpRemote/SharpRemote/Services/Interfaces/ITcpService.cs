b6248d4e5acb33495b7e145e3c05e1afec0c7e0e
Andrew <ahoffman@venadotech.com>
2019-12-27 10:00:47 -0600

    Extract Tcp logic into service

---
+++ SharpRemote/SharpRemote/Services/Interfaces/ITcpService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace SharpRemote.Services.Interfaces
+{
+    public interface ITcpService
+    {
+        Task ConnectToClientAsync();
+        Task DisconnecFromClientAsync();
+        Task WriteAsync(byte[] buffer, int offset, int count);
+        Task WriteAsync(string data);
+    }
+}
---
+++ SharpRemote/SharpRemote/Services/TcpService.cs
@@ -0,0 +1,50 @@
+﻿using SharpRemote.Services;
+using SharpRemote.Services.Interfaces;
+using Sockets.Plugin;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(TcpService))]
+namespace SharpRemote.Services
+{
+    public class TcpService : ITcpService
+    {
+        private readonly TcpSocketClient client;
+
+        public TcpService()
+        {
+            client = new TcpSocketClient();
+        }
+
+        public async Task ConnectToClientAsync()
+        {
+            // TODO: Allow dynamic entry.
+            const string ADDRESS = "192.168.1.123";
+            const int PORT = 10002;
+
+            await client.ConnectAsync(ADDRESS, PORT);
+        }
+
+        public async Task DisconnecFromClientAsync()
+        {
+            if (client.Socket.Connected)
+            {
+                await client.DisconnectAsync();
+            }
+        }
+
+        public async Task WriteAsync(string data)
+        {
+            var bytes = Encoding.ASCII.GetBytes(data);
+
+            await WriteAsync(bytes, 0, bytes.Length);
+        }
+
+        public async Task WriteAsync(byte[] buffer, int offset, int count)
+        {
+            await client.WriteStream.WriteAsync(buffer, offset, count);
+            await client.WriteStream.FlushAsync();
+        }
+    }
+}
