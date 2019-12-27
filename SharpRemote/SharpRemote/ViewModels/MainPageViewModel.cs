096460a249f13344834e547def2f7192e1f1eae3
Andrew <ahoffman@venadotech.com>
2019-12-27 10:01:15 -0600

    Refactor MainPageViewModel to use new TcpService

--- SharpRemote/SharpRemote/ViewModels/MainPageViewModel.cs
+++ SharpRemote/SharpRemote/ViewModels/MainPageViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using SharpRemote.Services.Interfaces;
-using Sockets.Plugin;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -11,38 +8,33 @@ namespace SharpRemote.ViewModels
 {
     public class MainPageViewModel
     {
+        private readonly ITcpService tcpService;
         private readonly IVibrateService vibrateService;
 
         public MainPageViewModel()
         {
+            tcpService = DependencyService.Resolve<ITcpService>();
             vibrateService = DependencyService.Resolve<IVibrateService>();
+
             ButtonPressedCommand = new Command<string>(async (parameter) => await OnButtonPressed(parameter));
         }
 
         public ICommand ButtonPressedCommand { get; private set; }
 
         private async Task OnButtonPressed(string parameter)
-        {
-            vibrateService.Vibrate(50);
-            var address = "192.168.1.123";
-            var port = 10002;
-            var command = $"RCKY{parameter}  \r";
+        {                  
+            try
+            {
+                vibrateService.Vibrate(50);
 
+                var command = $"RCKY{parameter}  \r";
 
-            var client = new TcpSocketClient();
-            await client.ConnectAsync(address, port);
+                await tcpService.WriteAsync(command);
+            }
+            catch (Exception)
+            {
 
-            // we're connected!
-
-            var bytes = Encoding.ASCII.GetBytes(command);
-
-            // write to the 'WriteStream' property of the socket client to send data
-            await client.WriteStream.WriteAsync(bytes, 0, bytes.Length);
-            await client.WriteStream.FlushAsync();
-
-
-            var thing = client.GetConnectedInterfaceAsync();
-            await client.DisconnectAsync();
-        }
+            }            
+        }        
     }
 }
