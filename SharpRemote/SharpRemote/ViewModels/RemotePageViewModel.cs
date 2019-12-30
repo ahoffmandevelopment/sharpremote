6988b46baf7f5fc6a81b10dd5a605d45feec0a17
Andrew <ahoffman@venadotech.com>
2019-12-30 12:44:36 -0600

    Separate power button into on/off and wire up correctly

--- SharpRemote/SharpRemote/ViewModels/RemotePageViewModel.cs
+++ SharpRemote/SharpRemote/ViewModels/RemotePageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Rg.Plugins.Popup.Contracts;
 using Rg.Plugins.Popup.Services;
+using SharpRemote.Extensions;
 using SharpRemote.Views;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -20,12 +21,22 @@ namespace SharpRemote.ViewModels
 
         public ICommand KeypadPressedCommand { get; private set; }
 
+        public ICommand PowerOnPressedCommand { get; private set; }
+
         public ICommand RefreshConnectionPressedCommand { get; private set; }        
         
         private async Task OnKeypadPressedAsync()
         {
             vibrateService.Vibrate(50);
             await popupNavigation.PushAsync(new KeypadPage(), false);
+        }
+
+        private async Task OnPowerOnPressedAsync()
+        {
+            vibrateService.Vibrate(50);
+            await tcpService.WriteAsync("RSPW1".ToSharpCommandString());
+            await Task.Delay(300);
+            await tcpService.WriteAsync("POWR1".ToSharpCommandString());
         }
 
         private async Task OnRefreshConnectionPressedAsync()
@@ -37,6 +48,7 @@ namespace SharpRemote.ViewModels
         private void WireupCommands()
         {            
             KeypadPressedCommand = new Command(async () => await OnKeypadPressedAsync());
+            PowerOnPressedCommand = new Command(async () => await OnPowerOnPressedAsync());
             RefreshConnectionPressedCommand = new Command(async () => await OnRefreshConnectionPressedAsync());
         }
     }
