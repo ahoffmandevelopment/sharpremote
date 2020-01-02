3ff606a405c45155ccd71bd88bc4166011c4c94c
Andrew <ahoffman@venadotech.com>
2020-01-02 09:45:55 -0600

    Add null check in StringExtensions.ToSharpCommandString method.

--- SharpRemote/SharpRemote/Extensions/StringExtensions.cs
+++ SharpRemote/SharpRemote/Extensions/StringExtensions.cs
@@ -1,9 +1,22 @@
-﻿namespace SharpRemote.Extensions
+﻿using System;
+
+namespace SharpRemote.Extensions
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Takes a string, right pads it to a length of 8, and adds a carriage return.
+        /// </summary>
+        /// <param name="input">The string to format.</param>
+        /// <returns>The formatted command string.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if input string is null.</exception>
         public static string ToSharpCommandString(this string input)
         {
+            if (input is null)
+            {
+                throw new ArgumentNullException(nameof(input), "Input string cannot be null.");
+            }
+
             return $"{input.PadRight(8)}\r";
         }
     }
